11d4267ef29bc4cd4073ebeb5fa5717b4e9c22f5
azakapro <azizullo0077@gmail.com>
2026-09-23 12:42:15 +0500

    fix(router): use HashRouter for GitHub Pages SPA compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Chapter01Page from "./pages/Chapter01Page";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/ch01" element={<Chapter01Page />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
